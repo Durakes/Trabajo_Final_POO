22c649c0bbdf7966a907a61d1a0d227146310f1a
Arnold Pajuelo <arnold.pajuelo@gmail.com>
2022-06-22 19:32:25 -0500

    Update
    Data Prueba
    Auxiliar
    * Pantalla Menu ASCII

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,7 @@
 #include <vector>
 #include <conio.h>
 #include <algorithm>
+#include "..\include\Auxiliar.h"
 #include "..\include\Archive.h"
 #include "..\lib\bcrypt\src\bcrypt.cpp"
 #include "..\src\buscarCancion.cpp"
@@ -17,18 +18,29 @@ void menuOpciones(int code, string name, string typeUser)
     system("cls");
     if(typeUser == "user")
     {
-        cout << "##### Bienvenido "<< name << " ##### \n Playlist [1] \n Buscar Canciones [2] \n Buscar Artista [3] \n Ingrese una opcion [1-3] o presione [5] para salir" << endl;
+        aux::cuadro(0,0,45, 13);
+        //! hacer un string, conocer la cantidad de caracteres, dividir total/string size y restar - 1 para que esté centrado!
+        aux::gotoxy(5,1);   cout << "##### Bienvenido "<< name <<" ##### "; //! Convertir a string
+        aux::gotoxy(1,3);   cout << "Playlist" << endl;         aux::gotoxy(25,3); cout << "[1]" << endl;
+        aux::gotoxy(1,4);   cout << "Buscar Canciones" << endl; aux::gotoxy(25,4); cout << "[2]" << endl;
+        aux::gotoxy(1,5);   cout << "Buscar Artista" << endl;   aux::gotoxy(25,5); cout << "[3]" << endl;
+        aux::gotoxy(1,6);   cout << "Salir" << endl;            aux::gotoxy(25,6); cout << "[5]" << endl;
+        aux::gotoxy(1,8);   cout << "Ingrese la opcion que desea > "; cin >> option; cin.ignore();
     }else
     {
-        cout << "##### Bienvenido "<< name << " ##### \n Playlist [1] \n Buscar Canciones [2] \n Buscar Artista [3] \n Crear Cancion [4] \n Ingrese una opcion [1-4] o presione [5] para salir" << endl;
+        aux::cuadro(0,0,45, 13);
+        aux::gotoxy(6,1);   cout << "##### Bienvenido "<< name <<" ##### ";
+        aux::gotoxy(1,3);   cout << "Playlist";         aux::gotoxy(25,3); cout << "[1]";
+        aux::gotoxy(1,4);   cout << "Buscar Canciones"; aux::gotoxy(25,4); cout << "[2]";
+        aux::gotoxy(1,5);   cout << "Buscar Artista";   aux::gotoxy(25,5); cout << "[3]";
+        aux::gotoxy(1,6);   cout << "Crear Cancion";    aux::gotoxy(25,6); cout << "[4]";
+        aux::gotoxy(1,7);   cout << "Salir";            aux::gotoxy(25,7); cout << "[5]";
+        aux::gotoxy(1,9);   cout << "Ingrese la opcion que desea > "; cin >> option; cin.ignore();
     }
-
-    cin >> option;
-    cin.ignore();
-
     switch (option)
     {
     case 1:
+        system("cls");
         menuPlaylist(name);
         break;
     case 2:
@@ -36,14 +48,21 @@ void menuOpciones(int code, string name, string typeUser)
         menuBuscarCancion(name,"0");
         menuOpciones(code, name, typeUser);
         break;
+    case 3:
+        system("cls");
+        //función Evie;
+        break;
     case 4:
         system("cls");
         subMenu_CrearCancion(code, name);
+        menuOpciones(code, name, typeUser);
+        break;
     case 5:
         exit(0);
         break;
-
     default:
+        aux::gotoxy(1,11); cout << "Ingrese una opcion correcta!!" << endl;
+        menuOpciones(code, name, typeUser);
         break;
     }
 }
@@ -55,7 +74,7 @@ void login(int tries)
     int ch;
     string password;
     bool userExist;
-    string path = "..\\docs\\Users.bin";
+    string path = "..\\docs\\Usuarios.bin";
     BinaryFile binFile(path);
     vector<User> users = binFile.LeerDato();
     User usuario;
@@ -64,10 +83,10 @@ void login(int tries)
     if(tries<3)
     {
         system("cls");
-        cout << "Ingrese su Usuario: " << endl;
-        cin >> user;
-        cout << "Ingrese su Contrasena: " << endl;
-        ch = getch();
+        aux::cuadro(0,0,45, 11);
+        aux::gotoxy(9,1);   cout << "##### INICIO DE SESION #####" << endl;
+        aux::gotoxy(1,3);   cout << "Ingrese su Usuario > "; getline(cin, user);
+        aux::gotoxy(1,4);   cout << "Ingrese su Contrasena > "; ch = getch();
         while (ch != 13) /*13 ASCCI ENTER*/
         {
             if(ch != 8) /*8 BACKSPACE*/
@@ -102,24 +121,23 @@ void login(int tries)
                 menuOpciones(usuario.getCode(), usuario.getUsername(), usuario.getType());
             }else
             {
-                cout << endl;
-                cout << "Los datos ingresados son incorrectos!!" << endl;
-                system("pause");
+                
+                aux::gotoxy(1,6); cout << "Los datos ingresados son incorrectos!!" << endl;
+                aux::gotoxy(1,7); system("pause");
                 login(tries+1);
             }
         }else
         {
-            cout << endl;
-            cout << "El usuario no existe!" << endl;
-            system("pause");
+            
+            aux::gotoxy(1,6); cout << "El usuario no existe!" << endl;
+            aux::gotoxy(1,7); system("pause");
             login(tries+1);
         }
     }
     else
     {
-        cout << endl;
-        cout << "Ha superado el limite de intentos " << endl;
-        system("pause");
+        aux::gotoxy(1,9); cout << "Ha superado el limite de intentos " << endl;
+        aux::gotoxy(1,10); system("pause");
         exit(0);
     }
 
@@ -129,8 +147,6 @@ void login(int tries)
 void registerUser()
 {
     string name;
-    string email;
-    string phoneNumber;
     string username;
     string passwordIni;
     string passwordFin;
@@ -139,17 +155,13 @@ void registerUser()
     bool userExists = false;
 
     system("cls");
-    cout << " REGISTRO DE NUEVO USUARIO \n";
-    cout << "Ingrese su nombre completo: \n";
-    getline(cin, name);
-    cout << "Ingrese su usuario a usar: \n";
-    getline(cin, username);
-    cout << "Ingrese su contrasena: \n";
-    getline(cin, passwordIni);
-    cout << "Ingrese su contrasena nuevamente: \n";
-    getline(cin, passwordFin);
-    cout << "Es una cuenta de artista? (Si o no)\n";
-    getline(cin, typeArtist);
+    aux::cuadro(0,0,59,15);
+    aux::gotoxy(17,1);   cout << "##### NUEVO REGISTRO #####" << endl;
+    aux::gotoxy(1,3);   cout << "Ingrese su nombre completo > ";    getline(cin, name);
+    aux::gotoxy(1,4);   cout << "Ingrese su usuario a usar > ";     getline(cin, username);
+    aux::gotoxy(1,5);   cout << "Ingrese su contrasena > ";  getline(cin, passwordIni);
+    aux::gotoxy(1,6);   cout << "Ingrese su contrasena nuevamente > "; getline(cin, passwordFin);
+    aux::gotoxy(1,7);   cout << "Es una cuenta de artista? (Si o no) > "; getline(cin, typeArtist);
     transform(typeArtist.begin(), typeArtist.end(), typeArtist.begin(), ::tolower);
 
     //Arreglar y ordenar
@@ -161,11 +173,11 @@ void registerUser()
         type = "user";
     }
 
-    string path = "..\\docs\\Users.bin";
+    string path = "..\\docs\\Usuarios.bin";
     BinaryFile archive(path);
     User user;
     int code = 1;
-    string ruta = "../docs/Users.bin";
+    string ruta = "../docs/Usuarios.bin";
     ifstream file((char*)&ruta[0]);
     if(file.good() == true)
     {
@@ -182,9 +194,9 @@ void registerUser()
         {
             if(username == users[i].getUsername())
             {
-                cout << endl;
-                cout << "Este nombre de usuario ya existe" << endl;
-                system("pause");
+                
+                aux::gotoxy(1,9);   cout << "Este nombre de usuario ya existe" << endl;
+                aux::gotoxy(1,10);   system("pause");
                 userExists = true;
                 break;
             }else
@@ -201,29 +213,35 @@ void registerUser()
             string hashed = bcrypt::generateHash(passwordIni);
             User newUser(code, (char*)&name[0], (char*)&username[0], (char*)&hashed[0], (char*)&type[0]);
             archive.GrabarDato(newUser);
-            cout << endl;
-            cout << "Cuenta creada exitosamente!" << endl;
-            system("pause");
+            //cout << endl;
+            aux::gotoxy(1,9);  cout << "Cuenta creada exitosamente!" << endl;
+            aux::gotoxy(1,10);  system("pause");
         }else
         {
-            cout << endl;
-            cout << "Las contrasenas no coinciden!!!" << endl;
-            system("pause");
+            //cout << endl;
+            aux::gotoxy(1,9);   cout << "Las contrasenas no coinciden!!!" << endl;
+            aux::gotoxy(1,10);   system("pause");
             registerUser();
         }
     }else
     {
         registerUser();
     }
-
 }
 
 int main()
 {
     int option;
-    cout << "##### INICIO DE SESION ##### \n Iniciar sesion [1] \n Registro [2] \n Ingrese una opcion [1-2]" << endl;
-    cin >> option;
-    cin.ignore();
+    system("cls");
+    aux::cuadro(0,0,35, 10);
+    aux::gotoxy(7,1);   cout << "##### BIENVENIDO #####" << endl;
+    //cout << endl;
+    aux::gotoxy(1,3);   cout << "Iniciar sesion" << endl;   aux::gotoxy(18,3); cout << "[1]" << endl;
+    aux::gotoxy(1,4);   cout << "Registro" << endl;         aux::gotoxy(18,4); cout << "[2]" << endl;
+    aux::gotoxy(1,5);   cout << "Ingrese la opcion que desea > "; cin >> option; cin.ignore();
+    //cout << "##### INICIO DE SESION ##### \n Iniciar sesion [1] \n Registro [2] \n Ingrese una opcion [1-2]" << endl;
+    // cin >> option;
+    // cin.ignore();
 
     switch (option)
     {
